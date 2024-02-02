9075d91dde0ee43ecf3804d2db502d3b91224ffd
subhash131 <subhashnayak131@gmail.com>
2024-02-03 00:50:02 +0530

    add auto scroll

--- src/components/Chat/Conversation.tsx
+++ src/components/Chat/Conversation.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import { AIContext } from "@/context/StateContext";
+import React, { useContext, useEffect, useRef } from "react";
 
 const Conversation = ({ children }: { children: React.ReactNode }) => {
+  const context = useContext(AIContext);
+  const conversationRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (conversationRef.current) {
+      conversationRef.current.scrollTop = conversationRef.current.scrollHeight;
+    }
+  }, [context?.isLoading]);
+
   return (
-    <div className="flex-grow bg-red-00 px-10 pt-10 h-[90vh] overflow-y-scroll flex flex-col w-[90%] gap-6 text-sm">
+    <div
+      className="flex-grow bg-red-00 px-10 pt-10 h-[90vh] overflow-y-scroll flex flex-col w-[90%] gap-6 text-sm"
+      ref={conversationRef as React.MutableRefObject<HTMLDivElement>}
+    >
       {children}
     </div>
   );
