90ef298d9ae634cbdefcff0e6e43ebc2ec7f256e
subhash131 <subhashnayak131@gmail.com>
2024-02-03 00:54:31 +0530

    update state context

--- src/context/StateContext.tsx
+++ src/context/StateContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 import useGeminiPro from "@/hooks/gemini/useGeminiPro";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { createContext } from "react";
 
 interface AIContext {
@@ -10,39 +10,52 @@ interface AIContext {
   inputMessage: string;
   messages: MessageType[];
   handleSubmit: () => void;
+  isLoading: boolean;
 }
 
 type MessageType = {
   message: string;
   sender: string;
 };
-export const AIContext = createContext<AIContext | undefined>(undefined);
+export const AIContext = createContext<AIContext | null>(null);
 
 const ContextComp = ({ children }: { children: React.ReactNode }) => {
   const [inputMessage, setInputMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const [messages, setMessages] = useState([
     {
-      message: "Hi, this is subhash",
+      message: "Hi, this app is built by subhash",
       sender: "ai",
     },
     {
-      message: `python def greet_user(name): """Prints a greeting to the user.""" print(f"Hello, {name}!") # Get the user's name from the command line. user_name = input("What is your name? ") # Greet the user. greet_user(user_name)`,
+      message: "For work contact me at subhashnayak131@gmail.com",
+      sender: "ai",
+    },
+    {
+      message:
+        "This application uses Google's Gemini Pro Generative Model to generate the response",
+      sender: "ai",
+    },
+    {
+      message: "Give it a try...",
       sender: "ai",
     },
   ]);
 
-  const addMessage = ({ newMessage }: { newMessage: MessageType }) => {
-    setMessages((state) => [...state, newMessage]);
-    setInputMessage("");
-  };
-
   const geminiProGenerator = useGeminiPro();
 
+  const addMessage = ({ newMessage }: { newMessage: MessageType }) => {
+    setMessages((state) => [...state, newMessage]);
+    if (newMessage.sender === "user") setInputMessage("");
+  };
+
   const generateGeminiProResponse = async () => {
+    setIsLoading(true);
     const response = await geminiProGenerator({ query: inputMessage });
     const newMessage = { message: response, sender: "ai" };
     addMessage({ newMessage });
+    setIsLoading(false);
   };
 
   const handleMessageInput = (value: string) => {
@@ -54,10 +67,11 @@ const ContextComp = ({ children }: { children: React.ReactNode }) => {
   return (
     <AIContext.Provider
       value={{
-        inputMessage,
         messages,
-        handleSubmit,
+        isLoading,
+        inputMessage,
         addMessage,
+        handleSubmit,
         handleMessageInput,
         generateGeminiProResponse,
       }}
